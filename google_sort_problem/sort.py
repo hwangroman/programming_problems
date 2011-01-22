919ce4ff21be64871a5d3ea901cbc92e89a407a3
Roman Hwang <hwang.roman@gmail.com>
2011-01-22 14:15:06 +0900

    - Refactoring. Clarified relationship to Dijkstra algorithm

--- google_sort_problem/sort.py
+++ google_sort_problem/sort.py
@@ -1,6 +1,6 @@
 import Queue
 
-class SortEntry(object):
+class SortNode(object):
     
     array = []
     cost = 0
@@ -16,41 +16,76 @@ class SortEntry(object):
         return '%s, %d, %d' % (self.array, self.cost, self.pos)
 
 
+def find_unsorted_element(array):
+    '''
+    Returns index before first unsorted element in array.
+    Returns -1 if array is sorted.
+    '''
+    for i in xrange(0, len(array)-1):
+        current = array[i]
+        next = array[i+1]
+        if (current > next):
+            return i
+            
+    return -1 
+
+
+def is_sorted(node):
+    '''
+    Check if node is sorted.
+    '''
+    return find_unsorted_element(node.array) == -1
+
+
+def get_nodes(node):
+    '''
+    Generates new nodes to check.
+    '''
+    v1_idx = find_unsorted_element(node.array)
+    if (v1_idx == -1):
+        raise RuntimeError('Array is sorted')
+
+    v2_idx = v1_idx + 1
+
+    v2 = node.array[v2_idx]
+
+    # Decreasing number case
+    dnc = SortNode(node.array, node.cost, v2_idx)
+    j = v1_idx
+
+    # Decrease all previous number that bigger than v2
+    while(j >= 0 and dnc.array[j] > v2):
+        dnc.cost += dnc.array[j]-v2
+        dnc.array[j] = v2
+        j-=1
+
+    # Removing number case
+    rnc = SortNode(node.array, node.cost, v1_idx)
+    del rnc.array[v2_idx]
+    rnc.cost += node.array[v2_idx]
+
+    return (dnc.cost, dnc), (rnc.cost, rnc)
+
+
 def sort(A):
-    print 'Starting sort'
-    initial = SortEntry(A, 0, 0)
+    '''
+    Actual method to solve the problem.
+    Modification of Dijkstra shortest path algorithm.
+    '''
+    initial = SortNode(A, 0, 0)
     queue = Queue.PriorityQueue()
+
     queue.put((0, initial))
+
     while(True):
         p, next = queue.get()
         print 'Got from queue:', next
 
-        for i in xrange(next.pos, len(next.array)-1):
-            if (next.array[i] > next.array[i+1]):
-
-                v2 = next.array[i+1]
-
-                # Decreasing number case
-                dnc = SortEntry(next.array, next.cost, i+1)
-                j = i
-                while(j >= 0 and dnc.array[j] > v2):
-                    dnc.cost += dnc.array[j]-v2
-                    dnc.array[j] = v2
-                    j-=1
-
-                queue.put((dnc.cost, dnc))
-
-                # Removing number case
-                rnc = SortEntry(next.array, next.cost, i)
-                del rnc.array[i+1]
-                rnc.cost += next.array[i+1]
-
-                queue.put((rnc.cost, rnc))
-
-                break
-        else:
-            # If for loop was not break, 
-            # then array is sorted and the cost is minimum
+        if (is_sorted(next)):
             return next.array, next.cost
 
+        new_nodes = get_nodes(next)
+
+        for n in new_nodes:
+            queue.put(n)
 
